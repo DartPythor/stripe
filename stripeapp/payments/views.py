1fa62b729f907d58bd7bfb0861f1c3d1e7dfb91c
DarthByte <neverovm28@gmail.com>
2025-05-29 00:38:23 +0400

    fix: Remove unusing package

--- stripeapp/payments/views.py
+++ stripeapp/payments/views.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 import stripe
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 
 from payments.models import Item, Order
 
